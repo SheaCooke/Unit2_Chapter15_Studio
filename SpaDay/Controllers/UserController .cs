7ceeba565cc8464f908f8a4040acab2efcaff639
shea cooke <SheaSCooke@gmail.com>
2021-07-22 15:14:22 -0700

    finished studio requirements

---
+++ SpaDay/Controllers/UserController .cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Add()
+        { 
+            ViewBag.didNotMatch = false;
+            return View();
+        }
+
+        
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            
+            
+            if (newUser.PassWord == verify)
+            {
+                /*ViewBag.didNotMatch = false;*/
+                ViewBag.userName = newUser.UserName;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.userName = newUser.UserName;
+                ViewBag.email = newUser.Email;
+                ViewBag.didNotMatch = true;
+                ViewBag.error = "Passwords must match";
+                return View("add");
+            }
+        }
+    }
+}
